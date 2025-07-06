a29a02ab326b29de00b6ca807eb458529efa14e4
Yaakov Kopstein <ykopstein@gmail.com>
2025-07-06 11:41:35 -0400

    Route color info page for history

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom';
 import './App.css'
 import SwColorInfo from './components/SwColorInfo'
 import SwColorSelector from './components/SwColorSelector'
 
-function App() {
-  const [colorCode, setColorCode] = useState<string>('');
-  
-  return (
-    <>
+function SwColorInfoRoute() {
+    const { colorCode } = useParams<{ colorCode: string }>();
+    const navigate = useNavigate();
+
+    return (
+        <>
             <h2>Sherwin Williams Color Info</h2>
-            <SwColorSelector onSelect={colorCode => setColorCode(colorCode)} colorCode={colorCode}></SwColorSelector>
-            <SwColorInfo colorCode={colorCode} onColorLink={colorCode => setColorCode(colorCode)}></SwColorInfo>
-    </>
-  )
+            <SwColorSelector
+                colorCode={colorCode || ''}
+                onSelect={code => navigate(`/color/${code}`)}
+            />
+            <SwColorInfo
+                colorCode={colorCode || ''}
+                onColorLink={code => navigate(`/color/${code}`)}
+            />
+        </>
+    );
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/color/:colorCode" element={<SwColorInfoRoute />} />
+                <Route path="*" element={<SwColorInfoRoute />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App
