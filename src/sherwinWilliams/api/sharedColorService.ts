bb9b615fde954ff31b5677a4464573cd35a0ccaa
Yaakov Kopstein <ykopstein@gmail.com>
2025-07-07 23:16:14 -0400

    Cache colors

--- src/sherwinWilliams/api/sharedColorService.ts
+++ src/sherwinWilliams/api/sharedColorService.ts
@@ -1,6 +1,19 @@
 import { type SharedColorServiceColor } from "./types";
 
 export const getColorInfo = async (colorCode: string): Promise<SharedColorServiceColor | null> => {
+    const cached = tryGetColorInfoFromCache(colorCode);
+    if(cached !== false) return cached;
+
+    const colorInfo = await downloadColorInfo(colorCode);
+    if (colorInfo !== null) {
+        addToCache(colorCode, colorInfo);
+        return colorInfo;
+    }
+
+    return null;
+};
+
+const downloadColorInfo = async (colorCode: string): Promise<SharedColorServiceColor | null> => {
     const response = await fetch(`https://api.sherwin-williams.com/shared-color-service/color/byColorNumber/${colorCode}`);
     if (!response.ok) {
         console.error(`Failed to fetch color info for ${colorCode}: ${response.statusText}`);
@@ -11,3 +24,22 @@ export const getColorInfo = async (colorCode: string): Promise<SharedColorServic
 
     return json as SharedColorServiceColor;
 };
+
+const tryGetColorInfoFromCache = (colorCode: string): SharedColorServiceColor | false => {
+    const key = getCacheKey(colorCode);
+    
+    const cached = localStorage.getItem(key);
+    if (cached) {
+        return JSON.parse(cached) as SharedColorServiceColor;
+    
+    } else {
+        return false;
+    }
+}
+
+const addToCache = (colorCode: string, colorInfo: SharedColorServiceColor): void => {
+    const key = getCacheKey(colorCode);
+    localStorage.setItem(key, JSON.stringify(colorInfo));
+}
+
+const getCacheKey = (colorCode: string): string => `scs:color:${colorCode.toUpperCase()}`;
